90cbca934a9ffc1c1aace11bee1a9638934aecef
Miron Bykov <miron.bykov@kcl.ac.uk>
2022-12-08 16:34:05 +0000

    complete task

--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .forms import ThingForm
+
 
 def home(request):
+    #adding form to veiws
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
